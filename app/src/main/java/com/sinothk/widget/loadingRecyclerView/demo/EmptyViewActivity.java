7d8bf2b05ab07078ff9df8434f5ba0f8de20a994
LiangYT <381518188@qq.com>
2019-03-26 18:01:46 +0800

    升级环境！

--- app/src/main/java/com/sinothk/widget/loadingRecyclerView/demo/EmptyViewActivity.java
+++ app/src/main/java/com/sinothk/widget/loadingRecyclerView/demo/EmptyViewActivity.java
@@ -1,9 +1,11 @@
 package com.sinothk.widget.loadingRecyclerView.demo;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
+import android.widget.TextView;
 
 import com.sinothk.widget.loadingRecyclerView.LoadingRecyclerView;
 
@@ -16,7 +18,7 @@ public class EmptyViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emptyview);
 
-        LoadingRecyclerView mRecyclerView = this.findViewById(R.id.recyclerview);
+        final LoadingRecyclerView mRecyclerView = this.findViewById(R.id.recyclerview);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(layoutManager);
@@ -26,22 +28,30 @@ public class EmptyViewActivity extends AppCompatActivity {
                 .getDefaultRefreshHeaderView()
                 .setRefreshTimeVisible(true);
 
-
-        View mEmptyView = findViewById(R.id.text_empty);
+        final TextView mEmptyView = findViewById(R.id.text_empty);
+        mEmptyView.setText("加载中...");
         mRecyclerView.setEmptyView(mEmptyView);
 
-        //没有数据，触发emptyView
-        ArrayList<String> listData = new ArrayList<String>();
-//        listData.add("SINNOTHK1");
-//        listData.add("SINNOTHK2");
-//        listData.add("SINNOTHK3");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //没有数据，触发emptyView
+                        ArrayList<String> listData = new ArrayList<String>();
+                        //        listData.add("SINNOTHK1");
+                        //        listData.add("SINNOTHK2");
+                        //        listData.add("SINNOTHK3");
 
-        MyAdapter mAdapter = new MyAdapter(listData);
-        mRecyclerView.setAdapter(mAdapter);
+                        MyAdapter mAdapter = new MyAdapter(listData);
+                        mRecyclerView.setAdapter(mAdapter);
 
-
-
-
+                        mEmptyView.setText("暂无数据");
+                    }
+                });
+            }
+        }, 3000);
     }
 
 //    @Override
