27eba1ec09e073ee79ddc03cd73927f5c53377fc
LiangYT <381518188@qq.com>
2020-03-26 12:31:14 +0800

    添加刷新样式！

--- LoadingRecyclerView/src/main/java/com/sinothk/widget/loadingRecyclerView/LoadingRecyclerView.java
+++ LoadingRecyclerView/src/main/java/com/sinothk/widget/loadingRecyclerView/LoadingRecyclerView.java
@@ -1,7 +1,6 @@
 package com.sinothk.widget.loadingRecyclerView;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
